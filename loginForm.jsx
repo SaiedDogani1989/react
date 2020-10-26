abd73d5debe9fced2ed17629ce2629fc6adb8ee8
Saeid Dogani <73443560+SaiedDogani1989@users.noreply.github.com>
2020-10-26 11:14:50 +0330

    Login form
    you must have and import the form component

---
+++ loginForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Joi from 'joi-browser'
+import Form from './common/form';
+
+class LoginForm  extends Form {
+state = {
+    data: {username: "" , password: ""},
+    errors:{}
+      } ;
+
+schema = {username: Joi.string().required().label('Username'),
+          password: Joi.string().required().label('Password')}
+
+      
+      doSubmit = () => {
+          //call the server
+         console.log('Submitted')
+         
+           }
+      
+    render() { 
+        return (  
+<form onSubmit = {this.handleSubmit}>
+   {/* what is onSubmit ? we use that to call the server, save the changes and then redirect the user to diffrent page 
+            actually we use that to prevent the full page reload */}
+ {this.renderInput('username' , 'Username')}
+ {this.renderInput('password' , 'Password' ,'password')}
+{this.renderButton('Login')}
+</form> );    }
+}
+export default LoginForm;
